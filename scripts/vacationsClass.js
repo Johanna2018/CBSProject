73b319a3f1bb77a44645390796f41563732cf2c2
Silvia Triscova <Silvia@MacBook-Air-uzivatela-Silvia.local>
2018-11-06 09:44:41 +0100

    Some suggestions from Henrik for the displaying of results in vacationClass

--- scripts/vacationsClass.js
+++ scripts/vacationsClass.js
@@ -28,5 +28,19 @@ this.tags = tags;
 this.index = null;
 
 
- }   
+ }
+ 
+ //The following part is something Henrik suggested for displaying the results, where "something" the Id of the div,the html part I want to display
+ //createHTML()
+
+    
+    //return "<div>" + this.title + "</div>";
+
 }
+
+// var html = "";
+
+// for(i = 0; i... )
+//  html += vac[i].createHTML();
+
+// document.getElementById("something").innerHTML = html;
