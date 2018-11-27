25c1c18893e29f18adae545dcf7503474a510291
Silvia Triscova <Silvia@MacBook-Air-uzivatela-Silvia.local>
2018-11-27 21:44:28 +0100

    unifying commit

--- scripts/vacationsClass.js
+++ scripts/vacationsClass.js
@@ -10,16 +10,16 @@ class Vacation {
         this.isPublished = isPublished;
         this.tags = tags;
         this.index = null;
-        // || [] has been deleted, because it was defined in createVacation
         this.ratings = ratings;
         this.center = center;
         this.zoom = zoom;
     };
 
     // The reason this function is in this script is that later on in the discover when we call the vacations as objects,
-    // we want the functionality of the object to be already included, this way we are actually using the classes
+    // we want the functionality of the object to be already included, this way we are using the classes
     getAverageRatings() {
-        //In case we don't manage to overwrite this value later in the function, we need to return something else than undefined, rather a number 0
+        //In case we don't manage to overwrite this value later in the function, we need to return 
+        //something else than undefined - rather a number 0
         var avgRating = 0;
         if (this.ratings.length > 0) {
             var ratingAvgSum = 0;
@@ -30,6 +30,8 @@ class Vacation {
             avgRating = ratingAvgSum/this.ratings.length;
         }
         //toFixed is returning a string value, we need a number with two decimals
+        //https://www.w3schools.com/jsref/jsref_parsefloat.asp
+        //https://www.w3schools.com/jsref/jsref_tofixed.asp
         return parseFloat(avgRating.toFixed(2));
     }
 }
